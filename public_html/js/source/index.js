951e5de0f6db1153f67e3ba041c6581b4e049dca
Bernhard Prott <Bernhard.Prott@gmx.de>
2017-08-14 21:18:42 +0200

    react is really routing---

--- public_html/js/source/index.js
+++ public_html/js/source/index.js
@@ -1,13 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Redirect } from 'react-router';
+import { Link } from 'react-router-dom';
+import createHashHistory from 'history/es/createHashHistory';
 
-class Hello extends React.Component {
+const history = createHashHistory({
+  queryKey: false
+});
+
+export class HelloAgain extends React.Component {
   render() {
-    return <h1>React works!</h1>
+    return (<div>
+      <h1>Hello again</h1>
+      <Link to="/aboutthatall">About that all</Link>
+    </div>);
+  }
+}
+
+export class AboutThatAll extends React.Component {
+  
+  render() {
+    return (<div>
+      <h1>This is about all You want to know...</h1>
+      <Link to="/helloagain">Take me home, Contry Roads...</Link>
+    </div>);
   }
 }
 
 ReactDOM.render(
-  <Hello/>,
+  <Router history={history}>
+    <div>
+      <Redirect from="/" to="helloagain" />
+      <Route path="/helloagain" component={HelloAgain} />
+      <Route path="/aboutthatall" component={AboutThatAll} />
+    </div>
+  </Router>,
   document.getElementById('reactor')
 );
+
+/*ReactDOM.render(
+  <div>
+    <HelloAgain />
+    <AboutThatAll />
+  </div>,
+  document.getElementById('reactor')
+);*/
+
+/*ReactDOM.render(
+  <HelloAgain />,
+  document.getElementById('reactor')
+);*/
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 
 var express = require('express');
 var app = express();
+var serveStatic = require('serve-static');
 
-app.get('/schnurpel', function (req, res) {
+//var serve = serveStatic('./public_html', {'index': ['index.html', 'index.htm']});
+
+
+app.get('/natur', function (req, res) {
   res.redirect('http://naturexkursionen.net');
 });
 
@@ -10,11 +14,14 @@ app.get('/world', function (req, res) {
   res.send('Hello World!');
 });
 
-app.use(express.static('./public_html'));
+// viewed at http://localhost:8080
+app.get('/js/build/index.js', function(req, res) {
+  res.sendFile(__dirname + "/public_html/js/build/index.js");
+});
 
-app.get('*', function (req, res) {
-  res.redirect('/');
-})
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + "/public_html/index.html");
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
